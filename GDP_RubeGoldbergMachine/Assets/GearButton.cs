dd2eef862bb61658c82c67f054a11411d68ccffa
Jordan Bloomfield (s4921851) <s4921851@bournemouth.ac.uk>
2018-05-17 20:25:26 +0100

    GearRotation2

---
+++ GDP_RubeGoldbergMachine/Assets/GearButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearButton : MonoBehaviour {
+
+    public bool notmoving;
+    public GameObject gear;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if(notmoving)
+        {
+
+            HingeJoint gearhinge =(HingeJoint) gear.gameObject.GetComponent(typeof(HingeJoint));
+
+            gearhinge.useMotor = true;
+
+            notmoving = false;
+
+        }
+
+    }
+
+}

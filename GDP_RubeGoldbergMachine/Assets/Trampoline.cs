8f3d98fafe06d9e6cabd049228387d69e700a9ec
Jordan Bloomfield (s4921851) <s4921851@bournemouth.ac.uk>
2018-05-18 22:54:04 +0100

    Trampoline and Mario Theme

---
+++ GDP_RubeGoldbergMachine/Assets/Trampoline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour {
+    float trampoline = 20;
+    public Rigidbody actor;
+	void Start () {
+        actor = GetComponent<Rigidbody>();	
+	}
+	
+	void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.name == "Trampoline")
+        {
+            actor.velocity = new Vector3(0, trampoline, 0);
+        }
+	}
+}

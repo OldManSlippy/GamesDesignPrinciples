e262dc606319672fbd629160e4946275693743ad
Jordan Bloomfield (s4921851) <s4921851@bournemouth.ac.uk>
2018-05-21 00:19:21 +0100

    Seesaw

--- GDP_RubeGoldbergMachine/Assets/Elevator.cs
+++ GDP_RubeGoldbergMachine/Assets/Elevator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour {
     public GameObject lift;
-    public int time = 50;
+    public int time = 10;
     private bool on = false;
     private void OnTriggerEnter(Collider other)
     {
@@ -12,9 +12,9 @@ public class Elevator : MonoBehaviour {
     }
 	void Update ()
     {
-        if (on == true)
+        if (time >= 0)
         {
-            if (time >= 0)
+            if (on == true)
             {
                 Vector3 pos = lift.transform.position;
                 lift.transform.position = new Vector3(pos.x,pos.y + 1,pos.z);
